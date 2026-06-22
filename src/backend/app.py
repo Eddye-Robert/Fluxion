76008088574ed7451609bf41f6118306d3c9981e
gustavo <antoniogustavo0210@gmail.com>
2026-06-22 02:01:51 -0300

    aplicação de backend

---
+++ src/backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+transactions = []
+
+#listar transações
+@app.route("/transactions", methods=["GET"])
+def get_transactions():
+    return jsonify(transactions)
+
+#criar nova transação
+@app.route("/transactions", methods=["POST"])
+def add_transaction():
+    data = request.json
+    transactions.append(data)
+    return jsonify(data)
+
+#deletar transação
+@app.route("/transactions/<int:index>", methods=["DELETE"])
+def delete_transaction(index):
+    if 0 <= index < len(transactions):
+        transactions.pop(index)
+    return jsonify({"ok": True})
+
+app.run(debug=True)
